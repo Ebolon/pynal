6ca481e4148bdee32a80d9741655e5b55fa6940d
dominik <dominik.schacht@gmail.com>
2009-08-15 17:39:53 +0200

    Found better settings for pdf rendering.

--- src/prototypes/pdf/01PDF-Qt-copier.py
+++ src/prototypes/pdf/01PDF-Qt-copier.py
@@ -4,14 +4,25 @@
 # Args:
 #    1. <filename> - The pdf to load.
 #
-# Quality is shit but might improve when playing with parameters for
-# renderToImage(dpi, dpi), RenderHints, PrinterResoultion etc.
+# Quality is (no longer that much) shit but might improve when playing with
+# parameters for renderToImage(dpi, dpi), RenderHints, PrinterResoultion etc.
+#
+# Setting the DPI from the default of 72 to 120 (and higher) results in a
+# better picture. To fit the image on the pdf page it needs to be resized
+# to the size of the printer.getPage() QRect (which is the rect of the current
+# page.)
+#
+# Fine tuning the settings of this resizing might result in faster and/or better
+# results.
 #===============================================================================
 import sys
 
 import PyQt4.QtCore as QtCore
 import PyQt4.QtGui as QtGui
 import QtPoppler
+
+#dpi resolution used to render the pdf into a QImage.
+dpi = 120
 
 if len(sys.argv) < 2:
     print "Usage:\n\tpython 01PDF-Qt-copier.py <filename>"
@@ -31,10 +42,11 @@ print "Initialized printer. Output to: /tmp/output.pdf"
 painter = QtGui.QPainter(printer)
 print "Starting to print..."
 for i in range(0, document.numPages()):
-    image = document.page(i).renderToImage()
+    image = document.page(i).renderToImage(dpi, dpi)
     print "Size of page: " + str(image.width()) + "x"+ str(image.height())
     
-    painter.drawImage(0, 0, image)
+    #Draw the image on the page. Resize it to fit the complete page.
+    painter.drawImage(printer.pageRect(), image)
     printer.newPage()
     print "Printed page #" + str(i)
     
