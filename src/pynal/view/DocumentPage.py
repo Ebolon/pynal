6137bc0e875d379b3d7ef67535640fa5adf7f7b1
dominik <dominik.schacht@gmail.com>
2009-10-17 16:42:24 +0200

    Moved pdf rendering mechanism to DocumentPage. Still dirty and non-working.

--- src/pynal/view/DocumentPage.py
+++ src/pynal/view/DocumentPage.py
@@ -5,6 +5,9 @@ Doing this the java way seems extremely stupid and redundant...
 '''
 from PyQt4 import QtCore
 from PyQt4 import QtGui
+from PyQt4.QtCore import SIGNAL
+
+import pynal.models.Config as Config
 
 class DocumentPage(QtGui.QGraphicsItem):
     """
@@ -35,5 +38,52 @@ class DocumentPage(QtGui.QGraphicsItem):
     def paint(self, painter, option, widget=None):
         """
         Nothing to paint as all paintables are children if this item.
+
+        This method is used as the notification to start rendering this
+        page's background and pre-caching following/previous pages.
         """
-        pass
+        if self.background is None:
+            #TODO: send poppler-render job to ThreadPool?
+            self.loader = PdfLoaderThread(self)
+            self.loader.connect(self.loader, SIGNAL("output(QImage)"), self.background_ready)
+            self.loader.start()
+
+            #TODO: call paint on previous/next page to pre-cache.
+            pass
+        else:
+            pass
+
+    def background_ready(self, image):
+        self.loader = None #TODO: To destroy the thread object?
+        self.background = image
+        self.update()
+        """ something like this:
+        pixmap = QtGui.QPixmap.fromImage(image)
+        item = self.scene.addPixmap(pixmap)
+        item.setOffset(0, i*1500)
+        """
+
+class PdfLoaderThread(QtCore.QThread):
+    """
+    Create QImage for a given poppler page.
+
+    TODO: I still don't like creating a new thread for every page.
+    """
+    def __init__(self, page):
+        """
+        Create a new PdfLoaderThread.
+
+        parameters:
+            doc - the QtPoppler.Poppler.Document that is to be loaded.
+            scene - the QGraphicsScene that will receive the QImages.
+        """
+        QtCore.QThread.__init__(self)
+
+        self.page = page
+
+    def run(self):
+        """ Create the images and notify the QGraphicsScene. """
+        image = self.page.bg_source.renderToImage(Config.pdf_render_dpi,
+                                                   Config.pdf_render_dpi)
+        self.emit(SIGNAL("output(QImage)"), image)
+
--- src/pynal/view/PynalDocument.py
+++ src/pynal/view/PynalDocument.py
@@ -29,12 +29,6 @@ class PynalDocument(QtGui.QGraphicsView):
             self.document.setRenderHint(QtPoppler.Poppler.Document.Antialiasing and
                                         QtPoppler.Poppler.Document.TextAntialiasing)
 
-            #This can be removed when DocumentPage objects work.
-            self.thread = PdfLoaderThread(self.document, self.scene)
-            self.connect(self.thread, SIGNAL("output(QImage, int)"), self.addPage)
-
-            self.thread.start()
-
             # This might want to be moved into an own thread
             # (when numPages is over a certain threshold?)
             for i in range(0, self.document.numPages()):
@@ -59,44 +53,3 @@ class PynalDocument(QtGui.QGraphicsView):
         Create an empty page and append it to the end of the document.
         """
         self.pages.append(DocumentPage(self, bg_source))
-
-
-    def addPage(self, image, i):
-        """
-        Callback method for the worker thread.
-
-        Adds the created image as a pixmap to the scene.
-
-        TODO: This can be removed when DocumentPage objects work.
-        """
-        pixmap = QtGui.QPixmap.fromImage(image)
-        item = self.scene.addPixmap(pixmap)
-        item.setOffset(0, i*1500)
-
-class PdfLoaderThread(QtCore.QThread):
-    """
-    Creates QImages from all pages in a given Poppler document.
-
-    TODO: This can be removed when DocumentPage objects work.
-    """
-    def __init__(self, doc, scene):
-        """
-        Create a new PdfLoaderThread.
-
-        parameters:
-            doc - the QtPoppler.Poppler.Document that is to be loaded.
-            scene - the QGraphicsScene that will receive the QImages.
-        """
-        QtCore.QThread.__init__(self)
-
-        self.doc = doc
-        self.scene = scene
-
-    def run(self):
-        """ Create the images and notify the QGraphicsScene. """
-        for i in range(0, self.doc.numPages()):
-            image = self.doc.page(i).renderToImage(Config.pdf_render_dpi,
-                                                   Config.pdf_render_dpi)
-            self.emit(SIGNAL("output(QImage, int)"), image, i)
-            if i > 10:
-                break
