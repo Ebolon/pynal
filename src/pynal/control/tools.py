5d8f3c1b1cdeeb96ab80eb3b8d6e07df6e2ca361
dominik <dominik.schacht@gmail.com>
2010-03-27 18:00:07 +0100

    Added undo command object and basic implementation of adding an item via graphics tool.

--- src/pynal/control/tools.py
+++ src/pynal/control/tools.py
@@ -76,11 +76,14 @@ class PenTool(Tool):
         if page is None:
             return
 
+        # Create the QGraphicsItem that should be added to the page.
         page_point = page.mapFromScene(point_coords)
+        circle = QtGui.QGraphicsEllipseItem(page)
+        circle.setRect(QtCore.QRectF(page_point.x() - 20, page_point.y() - 20, 40, 40))
+        circle.setZValue(1)
 
-        item = document.scene().addEllipse(page_point.x() - 20, page_point.y() - 20, 40, 40)
-        item.setZValue(1)
-        item.setParentItem(page)
+        # Create and push the UndoCommand.
+        document.undo_stack.push(GraphicsItemCommand(document, circle))
 
     def mouseReleaseEvent(self, event, document):
         self.deviceDown = False
@@ -88,5 +91,40 @@ class PenTool(Tool):
     def mouseMoveEvent(self, event, document):
         if not self.deviceDown:
             return
+
+class GraphicsItemCommand(QtGui.QUndoCommand):
+    """
+    Base class for all undo commands that add an item to the page.
+    Allows easy undo/redo of adding the item to the scene and page.
+
+    Attributes:
+      document -- The document that provides the undo stack and the scene.
+      item     -- The item that is added to the document.
+      page     -- The page the item is on.
+    """
+
+    def __init__(self, document, graphicsItem):
+        """
+        Creates a new GraphicsItemCommand that can be pushed on an UndoStack.
+        Undo and redo changes the parent item of the graphicsItem. The parent
+        is read from the graphicsItem when the command is created therefore it
+        should have the parent before the command is created.
+
+        Parameters:
+          document     -- The document that contains the undo stack.
+          graphicsItem -- The QGraphicsItem that is added to the scene.
+        """
+        QtGui.QUndoCommand.__init__(self)
+        self.document = document
+        self.item = graphicsItem
+        self.page = graphicsItem.parentItem()
+
+    def undo(self):
+        """ Remove the item from the scene. """
+        self.document.scene().removeItem(self.item)
+
+    def redo(self):
+        """ Move the item into the scene with the page as a parent. """
+        self.item.setParentItem(self.page)
 
 current_tool = Tool()
